40b6eaa4914b21d66a030886e7e29c8bbcd827ff
PittTau <jil202@pitt.edu>
2022-12-24 16:52:52 -0500

    removed bg for all

--- alignment.py
+++ alignment.py
@@ -68,7 +68,7 @@ best_img = []
 best_img_T1 = []
 best_img_T2 = []
 
-for i in range(len(best_pos)):
+for i in range(len(best_pos)):    
     best_img_T1.append(rotated_T1[:,:,best_pos[i]])
     best_img_T2.append(rotated_T2[:,:,best_pos[i]])
 
@@ -96,27 +96,58 @@ for i in range(len(best_img_T1)):
 
 
 ###############################################################################
-# matcher = KF.LoFTR(pretrained='outdoor')
-# warpped_cam = []
-# for i in range(len(img_rgb)):
-#     input_dict = {"image0": K.color.rgb_to_grayscale(img_rgb[i]), # LofTR works on grayscale images only 
-#                 "image1": K.color.rgb_to_grayscale(best_img_T1[i])}
-#     with torch.inference_mode():
-#         correspondences = matcher(input_dict)
+img1_p = natsorted(glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/resizedCam/*.png'))
+img2_p = natsorted(glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/match_T1/*.png'))
+img3_p = natsorted(glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/match_T2/*.png'))
+
+
+img1 = [load_torch_image(imgp)[0]  for imgp in img1_p]
+img2 = [load_torch_image(imgp)[0]  for imgp in img2_p]
+img3 = [load_torch_image(imgp, False)[0]  for imgp in img3_p]
+
+img1_mask = [load_torch_image(imgp)[1]  for imgp in img1_p]
+img2_mask = [load_torch_image(imgp)[1]  for imgp in img2_p]
+
+img1_np = [cv2.imread(imgp)  for imgp in img1_p]
+
+matcher = KF.LoFTR(pretrained='outdoor')
+warpped_cam = []
+for i in tqdm(range(len(img1))):
+    input_dict = {"image0": K.color.rgb_to_grayscale(img1[i]), # LofTR works on grayscale images only 
+                "image1": K.color.rgb_to_grayscale(img2[i])}
+    with torch.inference_mode():
+        correspondences = matcher(input_dict)
     
-#     mkpts0 = correspondences['keypoints0'].cpu().numpy()
-#     mkpts1 = correspondences['keypoints1'].cpu().numpy()
-#     Fm, inliers = cv2.findFundamentalMat(mkpts0, mkpts1, cv2.USAC_MAGSAC, 0.1, 0.999, 100000)
-#     inliers = inliers > 0
+    mkpts0 = correspondences['keypoints0'].cpu().numpy()
+    mkpts1 = correspondences['keypoints1'].cpu().numpy()
+    Fm, inliers = cv2.findFundamentalMat(mkpts0, mkpts1, cv2.USAC_MAGSAC, 0.1, 0.999, 100000)
+    inliers = inliers > 0
 
-#     transform = cv2.estimateAffine2D(mkpts0, mkpts1)[0]
-#     transform
-#     _, _, cols, rows = best_img_T1[i].shape
-#     src = np.array(best_img_T1[i].detach().cpu())
-#     src = np.squeeze(src[0,0,:,:])
+    transform = cv2.estimateAffine2D(mkpts0, mkpts1)[0]
+    transform
+    _, _, cols, rows = img2[i].shape
+    src = np.array(img2[i].detach().cpu())
+    src = np.squeeze(src[0,0,:,:])
 
-#     target = np.array(img_rgb[i].detach().cpu())
-#     target = np.squeeze(target[0,0,:,:])
+    target = np.array(img1_np[i])
+    img_output = cv2.warpAffine(target, transform, (rows, cols))
+    warpped_cam.append(img_output)
 
-#     img_output = cv2.warpAffine(target, transform, (rows, cols))
-#     warpped_cam.append(img_output)
+try:
+    os.mkdir(f'{base_path}/{date}/{ID}/rembg_cam/resizedCam_adjusted')
+except:
+    pass
+
+for i in range(len(warpped_cam)):
+    img2[i] = np.array(np.squeeze(img2[i]).detach().cpu())
+    img3[i] = np.array(np.squeeze(img3[i]))
+    T1_img = img2[i]/np.max(img2[i]) * 255
+    T2_img = img3[i]/np.max(img3[i]) * 255
+    T1_img = np.transpose(T1_img, (1,2,0))
+    T2_img = np.transpose(T2_img, (1,2,0))[:,:,0]
+    T2_img = T2_img * (img2_mask[i]/np.max(img2_mask[i]))
+    resized_img = warpped_cam[i]/np.max(warpped_cam[i]) * 255
+    cv2.imwrite( f'{base_path}/{date}/{ID}/rembg_cam/match_T1/{i}.png', T1_img)
+    cv2.imwrite( f'{base_path}/{date}/{ID}/rembg_cam/match_T2/{i}.png', T2_img)
+    cv2.imwrite( f'{base_path}/{date}/{ID}/rembg_cam/resizedCam_adjusted/{i}.png', resized_img)
+    
