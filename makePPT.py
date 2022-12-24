291b72a3c552c1f216eb252c8c5062a7cba40806
PittTau <jil202@pitt.edu>
2022-12-24 14:24:35 -0500

    automatic script to make ppt

---
+++ makePPT.py
@@ -0,0 +1,51 @@
+import collections 
+import collections.abc
+from pptx import Presentation
+from pptx.util import Inches
+from pptx.util import Pt
+import glob
+import datetime as dt
+from natsort import natsorted, ns
+
+import pdb
+prs = Presentation()
+
+lyt=prs.slide_layouts[0] # choosing a slide layout
+slide=prs.slides.add_slide(lyt) # adding a slide
+title=slide.shapes.title # assigning a title
+
+subtitle=slide.placeholders[1] # placeholder for subtitle
+date='2022.12.19-22.22.47'
+ID='ADRC_58'
+CWID='CW22-45'
+title.text=f'{CWID}' # title
+subtitle.text=f'{date}' # subtitle
+
+slide.shapes.title.text_frame.paragraphs[0].font.size = Pt(100)
+
+prs.slide_width = Inches(16)
+prs.slide_height = Inches(9)
+
+base_path = '/Users/jinghangli/Library/CloudStorage/OneDrive-UniversityofPittsburgh/03-PMB/PMB_ADRC/'
+T1_path = glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/match_T1/*.png')
+T1_path = natsorted(T1_path)
+
+T2_path = glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/match_T2/*.png')
+T2_path = natsorted(T2_path)
+
+cam_path = glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/resizedCam/*.png')
+cam_path = natsorted(cam_path)
+
+ap_layout = glob.glob(f'{base_path}/{date}/{ID}/rembg_cam/ap/*.png')
+
+blank_slide_layout = prs.slide_layouts[6]
+slide = prs.slides.add_slide(blank_slide_layout)
+slide.shapes.add_picture(ap_layout[0], Inches(2.5), Inches(0.5))
+
+for ind in range(len(T1_path)):
+    slide = prs.slides.add_slide(blank_slide_layout)
+    slide.shapes.add_picture(T1_path[ind], Inches(1), Inches(2))
+    slide.shapes.add_picture(T2_path[ind], Inches(5), Inches(2))
+    slide.shapes.add_picture(cam_path[ind], Inches(9), Inches(2))
+# pdb.set_trace()
+prs.save(f"{base_path}{date}/{CWID}.pptx") # saving file
