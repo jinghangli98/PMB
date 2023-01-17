744b43a0ae5c1f3b41afbcba692d78579a210b13
jinghangli98 <jil202@pitt.edu>
2023-01-17 11:45:28 -0500

    update

--- mount_instruction.sh
+++ mount_instruction.sh
@@ -1,2 +1,4 @@
-sudo sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa jil202@136.142.190.89:/home/scans ~/scans
-#sudo umount ~/scans
+sudo umount /Volumes/storinator
+sudo mkdir /Volumes/storinator
+
+sudo sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa jil202@136.142.190.89:/home /Volumes/storinator
