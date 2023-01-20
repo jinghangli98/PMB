8bf051c56053fb03ad4ed7c9b1b650cab2bc4a6c
jinghangli98 <jil202@pitt.edu>
2023-01-20 15:08:18 -0500

    mount update

--- mount_instruction.sh
+++ mount_instruction.sh
@@ -1,4 +1,8 @@
-sudo umount /Volumes/storinator
-sudo mkdir /Volumes/storinator
+sudo diskutil umount force /Volumes/storinator
+sudo diskutil umount force /Volumes/crc
 
-sudo sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa jil202@136.142.190.89:/home /Volumes/storinator
+sudo mkdir /Volumes/storinator
+sudo mkdir /Volumes/crc
+
+sudo sshfs -o kill_on_unmount,reconnect,allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa jil202@136.142.190.89:/home /Volumes/storinator
+sudo sshfs -o kill_on_unmount,reconnect,allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa jil202@h2p.crc.pitt.edu: /Volumes/crc
